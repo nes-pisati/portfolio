3fc64ca099a93add85ff6660a99ab25cb7ca70f8
Vanessa Pisati <pisati.vanessa@gmail.com>
2026-02-15 17:53:15 +0100

    About refactor

--- src/components/homepage/About.tsx
+++ src/components/homepage/About.tsx
@@ -1,110 +1,53 @@
-import { useLayoutEffect, useRef } from "react";
+import { useRef } from "react";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useGSAP } from "@gsap/react";
 import BlackShade from "../ui-kit/BlackShade";
 
 gsap.registerPlugin(ScrollTrigger);
 
 const content = [
     {
-        text: "I’m Vanessa, a 30-year-old frontend developer. According to Leonardo Di Caprio, I’m past my expiration date — but for code, I’m still in excellent shape.",
+        text: "I’m Vanessa, a 30-year-old frontend developer. According to Leonardo Di Caprio, I’m past my expiration date — but for code, I’m still in excellent shape. ",
         img: "https://picsum.photos/400/400?random=1"
     },
     {
-        text: " I’m still a Padawan? Yes, but I’m curious, fast-learning, and constantly leveling up (bugs included).",
+        text: "I’m still a Padawan? Yes, but Anakin Skywalker was once one too and, like him, I'm constantly leveling up (bugs included).  ",
         img: "https://picsum.photos/400/400?random=2"
     },
     {
-        text: " My background in branding trained my eye long before my hands touched code, which means UI and UX naturally influence the way I build and think interfaces.",
+        text: "My background in branding trained my eye long before my hands touched code, which means UI and UX naturally influence the way I build and think interfaces. ",
         img: "https://picsum.photos/400/400?random=3"
     },
     {
-        text: " I like clean components, thoughtful interactions, and frontend code that doesn’t fight back when you open it six months later.",
+        text: "I like clean components, thoughtful interactions, and frontend code that doesn’t fight back when you open it six months later (hopefully). ",
         img: "https://picsum.photos/400/400?random=4"
     },
     {
-        text: " Constantly improving, mildly obsessed with polish, and never allergic to learning something new — but definitely allergic to graminaceae.",
+        text: "If you're wondering how I move within a team, I can only say this: this is how artificial intelligence feels working together with me. Need I say more?",
         img: "https://picsum.photos/400/400?random=5"
     }
 ];
+
+gsap.registerPlugin(ScrollTrigger);
 
 export default function About() {
     const container = useRef(null);
     const textsRef = useRef<(HTMLSpanElement | null)[]>([]);
     const imagesRef = useRef<(HTMLImageElement | null)[]>([]);
 
-    // useLayoutEffect(() => {
-    //     const ctx = gsap.context(() => {
-    //         textsRef.current.forEach((text, i) => {
-    //             const isFirst = i === 0;
-    //             ScrollTrigger.create({
-    //                 trigger: text,
-    //                 start: isFirst ? "top top" : "top 10%",
-    //                 end: "bottom 40%",
-    //                 onEnter: () => {
-    //                     gsap.to(text, { opacity: 1, duration: 0.5 });
-    //                     gsap.to(imagesRef.current[i], { opacity: 1, zIndex: 10, duration: 0.5 });
-    //                     imagesRef.current.forEach((img, j) => {
-    //                         if (i !== j) {
-    //                             gsap.to(img, { opacity: 0, zIndex: 1, duration: 0.5 });
-    //                         }
-    //                     });
-    //                     textsRef.current.forEach((t, j) => {
-    //                         if (i !== j) gsap.to(t, { opacity: 0.3, duration: 0.5 });
-    //                     })
-    //                 },
-    //                 onEnterBack: () => {
-    //                     gsap.to(text, { opacity: 1, duration: 0.5 });
-    //                     gsap.to(imagesRef.current[i], { opacity: 1, zIndex: 10, duration: 0.5 });
-    //                     textsRef.current.forEach((t, j) => {
-    //                         if (i !== j) gsap.to(t, { opacity: 0.3, duration: 0.5 });
-    //                     });
-    //                     imagesRef.current.forEach((img, j) => {
-    //                         if (i !== j) {
-    //                             gsap.to(img, { opacity: 0, zIndex: 1, duration: 0.5 });
-    //                         }
-    //                     });
-    //                 },
-    //                 onLeave: () => {
-    //                     gsap.to(text, { opacity: 0.3, duration: 0.5 });
-    //                 },
-    //                 onLeaveBack: () => {
-    //                     gsap.to(text, { opacity: 0.3, duration: 0.5 });
-    //                 },
-    //             });
-    //         });
-    //     }, container);
 
-    //     return () => ctx.revert();
-    // }, []);
 
     return (
-        <section ref={container} className="padding-x pt-10 md:grid grid-cols-5 gap-10 relative">
-            <div className="col-span-3 flex flex-col pt-[10vh] md:max-h-[80vh] max-h-[50vh]">
-                <BlackShade>
-                    <p className="about-text leading-tight">
-                        {content.map((item, i) => (
-                            <span
-                                key={i}
-                                ref={(el) => { textsRef.current[i] = el; }}
-                                className=""
-                            >
-                                {item.text}
-                            </span>
-                        ))}
-                    </p>
-                </BlackShade>
-            </div>
-            <div className="col-span-2 relative md:h-screen h-[50vh] sticky top-0 flex items-center justify-center">
-                {content.map((item, i) => (
-                    <img
-                        key={i}
-                        ref={(el) => { imagesRef.current[i] = el; }}
-                        src={item.img}
-                        alt=""
-                        className={`absolute w-full h-auto max-w-md object-cover rounded-lg shadow-lg transition-opacity duration-500 ${i === 0 ? "opacity-100 z-10" : "opacity-0 z-0"}`}
-                    />
-                ))}
+        <section ref={container} className="padding-x pt-10 gap-10 relative h-screen flex items-center">
+            <div>
+                <p className="about-text leading-tight">
+                    I’m Vanessa, a 30-year-old frontend developer.
+                    <br></br>According to Leonardo Di Caprio, I’m past my expiration date — but for code, I’m still in excellent shape.
+                    I’m still a Padawan? Yes, but Anakin Skywalker was once one too and, like him, I'm constantly leveling up (bugs included).
+                    <br></br>My background in branding trained my eye long before my hands touched code, which means UI and UX naturally influence the way I build and think interfaces.
+                    I like clean components, thoughtful interactions, and frontend code that doesn’t fight back when you open it six months later (hopefully).
+                </p>
             </div>
         </section>
     );
