e1e6a5fbbc28156da8bf27604fcde10203959331
Vanessa Pisati <pisati.vanessa@gmail.com>
2026-03-22 18:35:36 +0100

    Created footer navigation component

---
+++ src/components/project/FooterNavigation.tsx
@@ -0,0 +1,37 @@
+import { useTranslation } from "react-i18next";
+import { useNavigate, useParams } from "react-router-dom";
+import { projects } from "../../data/projects";
+
+export default function FooterNavigation() {
+    const navigate = useNavigate();
+    const { t } = useTranslation();
+
+    const { slug } = useParams<{ slug: string }>();
+    const currentProjectIndex = projects.findIndex((project) => project.slug === slug);
+
+    const handleNavigateBack = () => {
+        navigate('/home');
+    }
+
+    const handleNavigateNext = () => {
+        if (currentProjectIndex !== -1) {
+            const nextIndex = (currentProjectIndex + 1) % projects.length;
+            const nextProject = projects[nextIndex];
+            navigate(`/project/${nextProject.slug}`);
+            window.scrollTo(0, 0);
+        }
+    }
+
+    return (
+        <div className="py-20">
+            <div className="flex justify-between">
+                <div className="flex">
+                    <p className="menu-item" onClick={handleNavigateBack}>&lt; {t("goBack")}</p>
+                </div>
+                <div className="flex">
+                    <p className="menu-item" onClick={handleNavigateNext}>{t("goNext")} &gt;</p>
+                </div>
+            </div>
+        </div>
+    );
+}
